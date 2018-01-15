c7903c58507a37fb05aa58ca2e363f838434df87
Vesa Hagström <vesa@unity3d.com>
2018-01-15 20:07:14 +0200

    Cleaner scaling, less aggressive

--- src/Braille.hs
+++ src/Braille.hs
@@ -38,17 +38,21 @@ toBounds min max min' max' v =
 prettyBounds :: Double -> Double
 prettyBounds v =
   let
-    s = signum v
-    v' = abs v
-    t = 10 ^ (floor $ logBase 10.0 v') :: Int
-    n = t * (ceiling $ v' / fromIntegral t)
+    -- -3560
+    s = signum v -- -
+    v' = abs v -- 3560
+    base = floor $ logBase 10.0 v' -- 3
+    t = 10 ^ base :: Int -- 1000
+    t' = 10 ^ (max 0 $ base - 1) :: Int -- 100
+    m =  t * (floor $ v' / fromIntegral t) -- 3000
+    m' = t' * (ceiling $ (v' - fromIntegral m) / fromIntegral t') -- 3560-3000 => 560/100 => 5.6 => 6
   in
     if v' < 1 then
       v
     else if v' > 2 && v' < 10 then
       s * 10
     else
-      1.2 * s * fromIntegral (n)
+      1.1 * s * fromIntegral (m + m')
 
 setDots :: CanvasState -> Canvas
 setDots (CanvasState canvas w h ps xmin xmax ymin ymax) =
