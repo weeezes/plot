548b1e674103a6d56b19709b605e168d008f24b9
Vesa Hagström <vesa@unity3d.com>
2018-01-17 23:12:08 +0200

    smoke test with timeouts

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -4,4 +4,5 @@ set -e
 set -u
 
 stack build
-stack exec -- test-exe <(set -e; for i in {0..4000}; do echo "${i}   $(($RANDOM%60))"; done)
+timeout --foreground 12 stack exec -- test-exe <(set -e; for i in {0..4000}; do echo "${i}   $(($RANDOM%60))"; done) || true
+timeout --foreground 12 stack exec -- test-exe -s -- <(set -e; for i in {0..4000}; do echo "$(($RANDOM%60))"; done) || true
