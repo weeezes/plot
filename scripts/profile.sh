e1b12451189b1d8b3d6a8c2ad224e8131679690a
Vesa Hagström <vesa@unity3d.com>
2018-01-20 00:18:52 +0200

    No clear way on how to get profiling out if sigtermed

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -2,4 +2,4 @@
 
 stack build --profile --executable-profiling
 bin=$(find .stack-work/install -name "plot" | xargs ls -1t | head -n 1)
-timeout --foreground 60 $bin <(set -e; for i in {0..4000}; do echo "${i}   $(($RANDOM%60))"; done) +RTS -p 
+$bin <(set -e; for i in {0..4000}; do echo "${i}   $(($RANDOM%60))"; done) +RTS -p
