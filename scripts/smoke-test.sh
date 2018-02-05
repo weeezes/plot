6aeafb3284275ead0c0f7430fae200b100289349
Vesa Hagström <vesa@unity3d.com>
2018-02-05 16:16:27 +0200

    smoke-test single value data

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -5,6 +5,7 @@ set -u
 
 stack build
 bin=$(find .stack-work/install -name "plot" | xargs ls -1t | head -n 1)
+timeout --foreground 2 $bin <(set -e; for i in {0..40000}; do echo "${i}"; done) || true
 timeout --foreground 2 $bin <(set -e; for i in {0..40000}; do echo "${i}   $((-30 + $RANDOM%60))"; done) || true
 timeout --foreground 2 $bin <(set -e; for i in {0..40000}; do echo "$((-30 + $RANDOM%60))"; done) || true
 timeout --foreground 2 $bin <(set -e; for i in {0..40000}; do echo "${i} ;  $((-30 + $RANDOM%60))"; done) || true
