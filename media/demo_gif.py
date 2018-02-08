e268bd0d5722ce37565839a88d8193cfacab412a
Vesa H <weeezes@gmail.com>
2018-02-08 23:31:13 +0200

    make gif with demo_gif.py

--- media/demo_gif.py
+++ media/demo_gif.py
@@ -1,6 +1,14 @@
 from moviepy.editor import *
+import moviepy.video.tools.drawing as dw
 
-clip = VideoFileClip("plot_demo.mp4")
-#        .subclip((1,1),(1,3))
-#        .resize(0.3))
-#clip.write_gif("use_your_head.gif")
+clip = VideoFileClip("plot_demo.mp4").set_pos(("left","bottom"))
+size = clip.size
+clip = clip.crop(x1=0,y1=0,x2=size[0]-2,y2=size[1])
+
+bar = ImageClip("bar.png").to_RGB().set_pos(("left","top")).set_duration(clip.duration)
+
+final_size = (clip.size[0],clip.size[1]+bar.size[1])
+
+final = CompositeVideoClip([bar,clip],size=final_size)
+final.write_gif("plot_demo.gif", program="ImageMagick", opt="optimizeplus")
+#final.write_videofile("plot_demo_bar.mp4", fps=10, audio=False)
